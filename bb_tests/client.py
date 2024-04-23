fc4d89943ffcde302e0ae04c62adcaf9867b62df
guqiang <guq@openbook.com.cn>
2024-04-23 11:33:26 +0800

    add 'patch_http_client_with_timeout'

--- bb_tests/client.py
+++ bb_tests/client.py
@@ -8,12 +8,12 @@ from a3consul.scene_cases.watch_nodes import NodeThread
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format="{asctime} {levelname} {process:d} [{threadName}] {name} : {message}", style="{")
     node_thread_list = list()
-    for i in range(5):
+    for i in range(1):
         node_thread = NodeThread(
             topic="测试",
             conf={
                 "init": {
-                    "host": "127.0.0.1",
+                    "host": "192.168.5.14",
                     "port": 8501,
                     "scheme": "https",
                     "verify": "/data/ssl/ca.pem",
@@ -23,6 +23,7 @@ if __name__ == '__main__':
                     "ttl": 10,
                 },
                 "renew": {
+                    "per_request_timeout_seconds": 3,
                     "sleep_seconds": 6,
                     "timeout_seconds": 19,
                 }
@@ -36,4 +37,13 @@ if __name__ == '__main__':
     for t in node_thread_list:
         t.start()
 
-    time.sleep(10)
+    # # 测试不影响其它 requests
+    # import requests
+    # for i in range(10):
+    #     client = requests.session()
+    #     response = client.put('https://www.jd.com')
+    #     print(len(response.text))
+    #     time.sleep(3)
+
+    for t in node_thread_list:
+        t.join()
