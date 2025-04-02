f7e69351a9353f085e9143b0aa1d755f13f4ee14
three-kinds <3179158552@qq.com>
2025-04-02 08:03:09 +0800

    Update Node.

--- a3consul/scene_cases/node_discovery/node.py
+++ a3consul/scene_cases/node_discovery/node.py
@@ -4,7 +4,6 @@ import os
 import signal
 import time
 import uuid
-from dataclasses import dataclass
 from typing import Optional
 from threading import Thread
 
@@ -13,34 +12,25 @@ from consul import Consul
 from a3consul.patch_utils import patch_http_client_request_with_timeout
 
 
-@dataclass
-class NodeConf:
-    topic: str
-    init: dict
-    session: dict
-    renew: dict
-    node_path: str
-
-
 class Node:
     logger = logging.getLogger(__name__)
 
-    def __init__(self, conf: NodeConf):
+    def __init__(self, conf: dict):
         self._conf = conf
         self._client: Optional[Consul] = None
         self._session_id: Optional[str] = None
         self._renew_thread: Optional[Thread] = None
 
     def register_node_id(self) -> str:
-        self._client = Consul(**self._conf.init)
-        self._session_id = self._client.session.create(**self._conf.session, behavior='delete')
+        self._client = Consul(**self._conf["init"])
+        self._session_id = self._client.session.create(**self._conf["session"], behavior='delete')
 
         node_name = uuid.uuid4().hex
-        nodes_path = f'{self._conf.node_path}{node_name}'
-        node_id = nodes_path.rsplit('/', 2)[-1]
+        node_path = f'{self._conf["node_path"]}{node_name}'
+        node_id = node_path.rsplit('/', 2)[-1]
 
-        self._client.kv.put(key=nodes_path, value='', acquire=self._session_id)
-        self.logger.info(f'[{self._conf.topic}]Register node id: {node_id}.')
+        self._client.kv.put(key=node_path, value='', acquire=self._session_id)
+        self.logger.info(f'[{self._conf["topic"]}]Register node id: {node_id}.')
         return node_id
 
     def _renew_session(self, timeout_seconds: float) -> bool:
@@ -48,10 +38,10 @@ class Node:
         while True:
             try:
                 self._client.session.renew(session_id=self._session_id)
-                self.logger.debug(f'[{self._conf.topic}]Renew session success.')
+                self.logger.debug(f'[{self._conf["topic"]}]Renew session success.')
                 return True
             except Exception as e:
-                self.logger.warning(f'[{self._conf.topic}]Renew error: {e}.')
+                self.logger.warning(f'[{self._conf["topic"]}]Renew error: {e}.')
 
             # Retry once per second after an exception occurs.
             time.sleep(1)
@@ -59,7 +49,7 @@ class Node:
                 return False
 
     def _renew_thread_func(self):
-        conf = self._conf.renew
+        conf = self._conf["renew"]
         per_request_timeout_seconds = conf.get('per_request_timeout_seconds', None) or 2
         patch_http_client_request_with_timeout(self._client, per_request_timeout_seconds)
 
@@ -71,13 +61,13 @@ class Node:
             else:
                 break
 
-        self.logger.critical(f'[{self._conf.topic}]Renew failed.')
-        self.on_renew_failed()
+        self.logger.critical(f'[{self._conf["topic"]}]Renew failed.')
+        self._on_renew_failed()
 
     def start_renew_thread(self):
         self._renew_thread = Thread(target=self._renew_thread_func, daemon=True)
         self._renew_thread.start()
 
-    def on_renew_failed(self):
-        self.logger.critical(f'[{self._conf.topic}]Send SIGTERM to self...')
+    def _on_renew_failed(self):
+        self.logger.critical(f'[{self._conf["topic"]}]Send SIGTERM to self...')
         os.kill(os.getpid(), signal.SIGTERM)
